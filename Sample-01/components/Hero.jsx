196fdc9460a1434ae4d08b822ac190e65f4deb32
Sam Yapkowitz <samyapkowitz@gmail.com>
2023-06-08 19:20:30 -0500

    UI fixes

--- Sample-01/components/Hero.jsx
+++ Sample-01/components/Hero.jsx
@@ -6,12 +6,11 @@ const Hero = () => (
   <div className="hero my-5 text-center" data-testid="hero">
     <Logo testId="hero-logo" />
     <h1 className="mb-4" data-testid="hero-title">
-      Next.js Sample Project
+      Universal Login A/B Testing
     </h1>
 
     <p className="lead" data-testid="hero-lead">
-      This is a sample application that demonstrates an authentication flow for a Regular Web App, using{' '}
-      <a href="https://nextjs.org">Next.js</a>
+      This is a sample application that demonstrates how to A/B test the Auth0 Universal Login by using Next.JS edge middleware and Google Optimize
     </p>
   </div>
 );
--- Sample-01/pages/index.jsx
+++ Sample-01/pages/index.jsx
@@ -8,7 +8,7 @@ export default function Index() {
     <>
       <Hero />
       <hr />
-      <Content />
+      {/* <Content /> */}
     </>
   );
 }
