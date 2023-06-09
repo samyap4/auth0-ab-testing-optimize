90587077fd905e27cd18c7292f059aa1e5e25f5b
Sam Yapkowitz <samyapkowitz@gmail.com>
2023-06-08 19:29:32 -0500

    v2

--- Sample-01/pages/index.jsx
+++ Sample-01/pages/index.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import Hero from '../components/Hero';
-import Content from '../components/Content';
+import { COOKIE_NAME } from 'lib/constants'
+import { useGa } from 'lib/useGa'
+import Cookies from 'js-cookie'
+import { Text, Button } from '@vercel/examples-ui'
 
 export default function Index() {
+
+  const ga = useGa()
+  const [cookie, setCookie] = useState('')
+  const removeCookie = () => {
+    Cookies.remove(COOKIE_NAME)
+    window.location.reload()
+  }
+
+  useEffect(() => {
+    setCookie(Cookies.get(COOKIE_NAME))
+  }, [])
+
+  useEffect(() => {
+    if (ga && cookie) {
+      ga('set', 'exp', cookie)
+    }
+    ga('send', 'pageview')
+  }, [ga, cookie])
+
   return (
     <>
       <Hero />
-      <hr />
-      {/* <Content /> */}
+      <Text className="mb-4">
+        Click the button below if you want to change the current variant (each
+        variant has a 33% chance)
+      </Text>
+      <Button variant="secondary" onClick={removeCookie}>
+        Remove cookie & reload
+      </Button>
     </>
   );
 }
